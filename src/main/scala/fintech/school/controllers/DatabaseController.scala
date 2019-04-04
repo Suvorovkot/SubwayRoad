c9ee10455e4b3c2e9b56ff94b96789cd69a2cf1f
Paul <suvorovkot@gmail.com>
2019-04-04 21:44:44 +0300

    Database controller get method for spans and transitions

--- src/main/scala/fintech/school/controllers/DatabaseController.scala
+++ src/main/scala/fintech/school/controllers/DatabaseController.scala
@@ -8,27 +8,30 @@ class DatabaseController {
   val dbConnectionUrl = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres"
   Database.forURL(dbConnectionUrl, driver = "org.postgresql.Driver") withSession {
     implicit session =>
+      val spans = TableQuery[Spans]
+      val transitions = TableQuery[Transitions]
 
-//            // SELECT * FROM spans
-//            spans.list foreach { row =>
-//              println("Span from id " + row._1 + " to " + row._2)
-//            }
+      //            // SELECT * FROM spans
+      //            spans.list foreach { row =>
+      //              println("Span from id " + row._1 + " to " + row._2)
+      //            }
 
       //      // SELECT * FROM spans WHERE fromStationId = '1'
       //      spans.filter(_.fromStationId === "1").list foreach { row =>
       //        println("Span which fromStationId is '1' has id "+row._1 )
       //      }
-//      val spans = TableQuery[Span]
-//      val edgeList = new EdgeList()
-//      spans.list foreach { row =>
-//        edgeList.addEdge(Edge(row._1, row._2, row._3))
-//      }
-//      println(edgeList)
-      val trans = TableQuery[Transition]
-      val transList = new EdgeList()
-      trans.list foreach { row =>
-        transList.addEdge(Edge(row._1, row._2, row._3))
+      def getTransitions: EdgeList ={
+        val edgeList = new EdgeList()
+        transitions.list.foldLeft(edgeList){case (list, element) => list.addEdge(Edge(element.fromStationId,element.toStationId,element.value))}
       }
-      println(transList)
+
+      def getSpans: EdgeList ={
+        val edgeList = new EdgeList()
+        spans.list.foldLeft(edgeList){case (list, element) => list.addEdge(Edge(element.fromStationId,element.toStationId,element.value))}
+      }
+
+//      val edgeList = new EdgeList()
+//      val l = transitions.list.foldLeft(edgeList){case (list, element) => list.addEdge(Edge(element.fromStationId,element.toStationId,element.value))}
+//      println(l.map)
   }
 }
--- src/main/scala/fintech/school/models/Tables.scala
+++ src/main/scala/fintech/school/models/Tables.scala
@@ -3,31 +3,25 @@ package fintech.school.models
 
 import scala.slick.driver.PostgresDriver.simple._
 
+//case class Station(id: Long, name: String, status: Status.Value, startWork: Integer, endWork: Integer, workloadId: Long, isTransition: Boolean)
+//case class Workload(levelPerHour: List[Integer])
+//case class Line(id: Long, metroId: Long, name: String, number: Integer, colour: String, amountOfStation: Integer)
+//case class Metro(id: Long, city: String, country: String)
 
+case class Span(fromStationId: Int, toStationId: Int, value: Int)
+case class Transition(fromStationId: Int, toStationId: Int, value: Int)
 
-case class Station(id: Long, name: String, status: Status.Value, startWork: Integer, endWork: Integer, workloadId: Long, isTransition: Boolean)
-case class Workload(levelPerHour: List[Integer])
-case class Line(id: Long, metroId: Long, name: String, number: Integer, colour: String, amountOfStation: Integer)
-case class Metro(id: Long, city: String, country: String)
-
-class Transition(tag: Tag) extends Table[(Int, Int, Int, Int, Int)](tag, "Transition"){
-  //(fromStationId: Long, fromLineId: Long, toStationId: Long, toLineId: Long, time: Integer)
-  def fromStationId = column[Int]("From_id")
-  def fromLineId = column[Int]("From_id")
-  def toStationId = column[Int]("To_id")
-  def toLineId = column[Int]("From_id")
-  def time = column[Int]("value")
-  def * = (fromStationId, fromLineId, toStationId, toLineId, time)
-}
-
-class Span(tag: Tag) extends Table[(Int, Int, Int)](tag, "Span"){
+class Spans(tag: Tag) extends Table[Span](tag, "Span"){
   def fromStationId = column[Int]("From_id")
   def toStationId = column[Int]("To_id")
   def time = column[Int]("value")
-  def * = (fromStationId, toStationId, time)
+  def * = (fromStationId, toStationId, time) <> (Span.tupled,Span.unapply)
 }
 
-
-object Status extends Enumeration{
-  val Opened, Closed, Process = Value
+class Transitions(tag: Tag) extends Table[Transition](tag, "Transition"){
+  def fromStationId = column[Int]("From_id")
+  def toStationId = column[Int]("To_id")
+  def time = column[Int]("value")
+  def * = (fromStationId,toStationId,time) <> (Transition.tupled,Transition.unapply)
 }
+
