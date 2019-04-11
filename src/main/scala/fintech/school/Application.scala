52c7b453f7017aeb04505a594b54d22930fa39ac
Yowzah <putnikovgeneral@gmail.com>
2019-04-11 20:32:45 +0300

    docker success

--- src/main/scala/fintech/school/Application.scala
+++ src/main/scala/fintech/school/Application.scala
@@ -3,12 +3,21 @@ package fintech.school
 import fintech.school.repositories._
 import org.json4s.jackson.Serialization
 import org.json4s.DefaultFormats
+import akka.actor.ActorSystem
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 object Application {
   implicit val serialization = Serialization
   implicit val format = DefaultFormats
 
   def main(args: Array[String]): Unit = {
-    println("Success")
+    implicit val actor = ActorSystem()
+    implicit val executionContext = actor.dispatcher
+
+//    val span = new Span()
+//    println(Await.ready(span.getAll, 5.seconds))
+    println("Success!")
   }
 }
