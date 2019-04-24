5bd0d98fa92427d8663ad0d4c03fe0324298c928
Suvorovkot <pssuvorov@mail.ru>
2019-04-25 00:38:57 +0300

    Timestamp again.

--- src/main/scala/fintech/school/models/Tables.scala
+++ src/main/scala/fintech/school/models/Tables.scala
@@ -1,19 +1,19 @@
 package fintech.school.models
 
-import java.sql.Time
+import java.sql.Timestamp
 
 
 case class Span(fromStationId: Int, toStationId: Int, value: Int)
 
 case class Transition(fromStationId: Int, toStationId: Int, value: Int)
 
-case class Station(lineId: Int, name: String, status: Int, startWork: Time, endWork: Time, id: Int)
+case class Station(lineId: Int, name: String, status: Int, startWork: Timestamp, endWork: Timestamp, id: Int)
 
 case class Line(name: String, number: Int, colour: String, id: Int)
 
-case class Workload(lineId: Int, time: Time, value: Int)
+case class Workload(lineId: Int, time: Timestamp, value: Int)
 
-case class StationParams(lineId: Int, name: String, status: Int, startWork: Time, endWork: Time)
+case class StationParams(lineId: Int, name: String, status: Int, startWork: Timestamp, endWork: Timestamp)
 
 case class LineParams(name: String, number: Int, colour: String)
 
